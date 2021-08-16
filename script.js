865a3b9fea190e765835c5dfab08009acb887e31
Karthick <81891148+Karthickhub1@users.noreply.github.com>
2021-08-16 17:27:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function myFunction(x) {
+    x.classList.toggle("change");
+  }
+
+
+  const toggleButton = document.querySelector('.toggle-menu');
+  const navBar = document.querySelector('.nav-bar');
+  toggleButton.addEventListener('click', () => {
+    navBar.classList.toggle('toggle');
+  });
+  
